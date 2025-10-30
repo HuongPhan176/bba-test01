ccd8ba8fd2e88ef75a1c2d7ff54c54b505a2f3db
test 01 - Phan Thi Huong <jayphanvn@gmail.com>
2025-10-30 13:40:50 +0700

    fix: add more option for challenge 05

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,13 +1,20 @@
 function findPairsDivisibleBy17() {
     const numbers = [];
     let count = 0;
-    for (let i = 1; i <= 100 ; i++) {
-        if (i % 17 === 0) {
-            numbers.push(i);
+    for (let i = 1; i <= 100; i++) {
+        //Option 1
+        if (i * 17 > 100) {
+            break;
         }
+        numbers.push(i * 17);
+
+        //Otiopn 2
+        // if (i % 17 === 0) {
+        //     numbers.push(i);
+        // }
     };
     for (let i = 0; i < numbers.length; i++) {
-        for (let j = i+1; j< numbers.length; j++) {
+        for (let j = i + 1; j < numbers.length; j++) {
             count += 1;
             console.log(`(${numbers[i]}, ${numbers[j]}) is a pair of numbers that is divisible by 17`);
         }
