67a1b208c20eb3cc0c6196be8f626468804542fb
test 01 - Phan Thi Huong <jayphanvn@gmail.com>
2025-11-01 20:32:01 +0700

    fix: lesson 4 - address mentor's comment for challenge 05

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,22 +1,32 @@
 function findPairsDivisibleBy17() {
-    const numbers = [];
+    // const numbers = [];
     let count = 0;
-    for (let i = 1; i <= 100; i++) {
-        //Option 1
-        if (i * 17 > 100) {
-            break;
-        }
-        numbers.push(i * 17);
+    // for (let i = 1; i <= 100; i++) {
+    //     //Option 1
+    //     if (i * 17 > 100) {
+    //         break;
+    //     }
+    //     numbers.push(i * 17);
 
-        //Otiopn 2
-        // if (i % 17 === 0) {
-        //     numbers.push(i);
-        // }
-    };
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            count += 1;
-            console.log(`(${numbers[i]}, ${numbers[j]}) is a pair of numbers that is divisible by 17`);
+    //     //Otiopn 2
+    //     // if (i % 17 === 0) {
+    //     //     numbers.push(i);
+    //     // }
+    // };
+    // for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = i + 1; j < numbers.length; j++) {
+    //         count += 1;
+    //         console.log(`(${numbers[i]}, ${numbers[j]}) is a pair of numbers that is divisible by 17`);
+    //     }
+    // };
+    // console.log(`Total ${count} pairs of numbers divisible by 17`)
+
+    for (let i = 1; i < 100; i++) {
+        for (let j = i + 1; j <= 100; j++) {
+            if ((i + j) % 17 === 0) {
+                console.log(`(${i}, ${j}) is a pair of numbers that is divisible by 17 `);
+                count += 1;
+            }
         }
     };
     console.log(`Total ${count} pairs of numbers divisible by 17`)
